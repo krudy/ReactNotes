f3eef98e013ff99366102f017fe0bf437daa7c5a
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-02-10 16:11:01 +0100

    GET actions added

--- actions/api/noteActions.js
+++ actions/api/noteActions.js
@@ -2,7 +2,7 @@ const Note = require("../../db/models/note");
 
 class NoteActions {
 
-    saveNote (req, res) {
+    saveNote(req, res) {
 
         const title = req.body.title;
         const body = req.body.body;
@@ -10,21 +10,32 @@ class NoteActions {
         res.send('note added successfully. title: ' + title + ' body: ' + body);
     }
 
-    getAllNotes(req, res) {
-        // getting all notes
-        res.send('Api is working.');
+    // getting all notes
+    async getAllNotes(req, res) {
+
+        let notes;
+        try {
+            notes = await Note.find({});
+
+        } catch (err) {
+            return res.status(500).json({ message: err.message });
+        }
+        console.log(notes);
+        res.status(200).json(notes);
     }
-    
-    getNote(req, res) {
-        // getting a single note
-        res.send('note info');
+
+    // getting a single note
+    async getNote(req, res) {
+        const id = req.params.id;
+        const note = await Note.findOne({_id: id}); 
+        res.status(200).json(note);
     }
-    
+
     updateNote(req, res) {
         // updating a single note
         res.send('note has been updated');
     }
-    
+
     deleteNote(req, res) {
         const id = req.params.id;
         // deleting a single note
